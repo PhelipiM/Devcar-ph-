30fefd01b012bfeef1700c9ed058f0e3b1dfecde
phelipim <ggizmeuamigo@gmail.com>
2023-08-14 19:50:20 -0300

    Devcar(ph)

---
+++ src/routes.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import Busca from './screens/Busca';
+import Perfil from './screens/Perfil';
+import Pagamentos from './screens/Pagamentos';
+
+const BottomTab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator 
+        screenOptions={{
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'black',
+        }}
+      >
+        <BottomTab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Busca"
+          component={Busca}
+          options={{
+            tabBarLabel: 'Busca',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="search" color={color} size={26} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Perfil"
+          component={Perfil}
+          options={{
+            tabBarLabel: 'Perfil',
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="person" color={color} size={26} />
+            ),
+          }}
+              />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const PerfilStack = createStackNavigator();
+
+function PerfilRoutes() {
+  return (
+    <PerfilStack.Navigator>
+      <PerfilStack.Screen name="Perfil" component={Perfil} />
+      <PerfilStack.Screen name="Pagamentos" component={Pagamentos} />
+    </PerfilStack.Navigator>
+  );
+} 
---
+++ src/screens/Perfil/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from 'react-native';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
+export default function Perfil({ navigation }) {
+  return (
+    <ScrollView style={styles.container}>
+      <ScrollView>
+        <TouchableOpacity
+          style={styles.option}
+          onPress={() => navigation.navigate('Compras')}
+        >
+          <MaterialCommunityIcons name="credit-card" size={35} color="#333" />
+          <ScrollView style={styles.info}>
+            <Text style={styles.title}>Compras</Text>
+            <Text style={styles.description}>Ver minhas compras</Text>
+          </ScrollView>
+          <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+        </TouchableOpacity>
+      </ScrollView>
+
+      <View style={styles.menuAdicional}>
+        <TouchableOpacity style={styles.opcoesAdicionais}>
+          <View style={styles.wrapper}>
+            <MaterialCommunityIcons name="lifebuoy" size={25} color="#CDC" />
+            <Text style={styles.optionName}>Ajuda</Text>
+          </View>
+          <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.opcoesAdicionais}>
+          <View style={styles.wrapper}>
+            <MaterialIcons name="settings" size={25} color="#CDC" />
+            <Text style={styles.optionName}>Configurações</Text>
+          </View>
+          <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  option: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginTop: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    border: 1,
+  },
+  info: {
+    marginLeft: 20,
+  },
+  title: {
+    color: '#333',
+    fontSize: 18,
+  },
+  description: {
+    fontSize: 16,
+    color: '#999',
+  },
+  menuAdicional: {
+    marginTop: 30,
+  },
+
+  opcoesAdicionais: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 0,
+    marginRight: 0,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    border: 1,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  optionName: {
+    marginLeft: 25,
+    color: '#ccc',
+    fontSize: 15,
+  },
+});
